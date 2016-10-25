d7873d3fec8922894fba1eca83d0f64dd9a73539
cvaldes <cvaldes@152.139.124.46>
2016-10-25 14:21:07 -0300

    clase migaraje

---
+++ src/migaraje/MiGaraje.java
@@ -0,0 +1,13 @@
+package migaraje;
+
+public class MiGaraje {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//array de vehiculos
+		Vehiculo[] vehiculos = new Vehiculo[5];
+		
+		
+	}
+
+}
